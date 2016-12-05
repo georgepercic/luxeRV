3fa4f9c305e8a8c7114fa556a19956de9cd8b150
George Percic <george.percic@gmail.com>
2016-12-05 23:14:25 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+website
+=======
+
+A Symfony project created on December 2, 2016, 9:05 pm.
---
+++ src/UserBundle/Entity/User.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: georgepercic
+ * Date: 05/12/2016
+ * Time: 21:16
+ */
+
+namespace UserBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+}
